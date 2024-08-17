d59aca59d2eaf2365c1e91eed3df0421ec612f0a
mamorshedalam <ma.morshedalampro@gmail.com>
2024-08-17 12:49:07 +0800

    header and footer build

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,6 @@ module.exports = {
       mono: ["ui-monospace", "SFMono-Regular"],
       caveat: ["Caveat", "ui-sans-serif", "system-ui"],
     },
-    screens: {
-      xs: "420px",
-      sm: "580px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      xxl: "1400px",
-    },
   },
   plugins: [],
 };
